c3d90f7e0192ce2d73a34613206301e3a4e14b7f
tommy_krisna <tommykrisna7@gmail.com>
2018-10-18 00:34:19 +0700

    create read update delete user

--- app.js
+++ app.js
@@ -1,7 +1,93 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('user_db', 'postgres', '12345678', {
+    dialect: 'postgres'
+});
+const app = express();
+const port = 3000;
+const Parser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const User = sequelize.import(__dirname + '/models/user');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(Parser.json());
+app.use(Parser.urlencoded({extended: true}));
+
+
+app.get('/', (req, res) => {
+    User.findAll().then(users => {
+        res.json({
+            status: 'success',
+            result: users
+        });
+    })
+
+});
+
+app.post('/api/user/add', (req, res) => {
+    console.log('req', req.body);
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        address: req.body.address
+    }).then(user => {
+        res.json({
+            status: 'success',
+            result: user
+        });
+        // console.log(user.get({
+        //     plain: true
+        // }));
+    }).catch(err => {
+        res.json({
+            status: 'Failed',
+            message: ''
+        });
+        console.log('err', err);
+    });
+});
+
+app.put('/api/user/edit', (req, res) => {
+    User.update({
+        name: req.body.name,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        address: req.body.address
+    }, {
+        where:{
+            userID: req.body.userID
+        }
+    }).then(user =>{
+        res.json({
+            status: 'success',
+            result: user
+        });
+    }).catch(err =>{
+        res.json({
+            status: 'Failed',
+            message: ''
+        });
+    })
+});
+
+
+app.delete('/api/user/delete', (req, res)=>{
+    User.destroy({
+        where:{
+            userID: req.body.userID
+        }
+    }).then(user =>{
+        res.json({
+            status: 'success',
+            result: user
+        });
+    }).catch(err =>{
+        res.json({
+            status: 'Failed',
+            message: ''
+        });
+    })
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- models/user.js
+++ models/user.js
@@ -1,14 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    userID: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    phoneNumber: DataTypes.STRING,
-    address: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
+    const User = sequelize.define('User', {
+        userID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        phoneNumber: DataTypes.STRING,
+        address: DataTypes.STRING
+    }, {});
+    User.associate = function (models) {
+        // associations can be defined here
+    };
+    return User;
 };
